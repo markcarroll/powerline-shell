fefb6f30112979847a3c54690469ed3923410f45
Mark Carroll <markc@signal4d.com>
2019-03-04 10:21:46 -0800

    Add cloud icon to AWS segment

--- powerline_shell/segments/aws_profile.py
+++ powerline_shell/segments/aws_profile.py
@@ -7,6 +7,6 @@ class Segment(BasicSegment):
         aws_profile = os.environ.get("AWS_PROFILE") or \
             os.environ.get("AWS_DEFAULT_PROFILE")
         if aws_profile:
-            self.powerline.append(" aws:%s " % os.path.basename(aws_profile),
+            self.powerline.append("☁️ aws:%s " % os.path.basename(aws_profile),
                                   self.powerline.theme.AWS_PROFILE_FG,
                                   self.powerline.theme.AWS_PROFILE_BG)
